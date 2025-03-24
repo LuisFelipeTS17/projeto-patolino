e88d3d6a813b226204d913a6963e3258e3991d06
Eric <ericsouza94895@gmail.com>
2025-03-23 23:38:57 -0300

    feat: add atributo alternativas para guardar as alternativas da pergunta

--- src/main/java/com/senac/model/Pergunta.java
+++ src/main/java/com/senac/model/Pergunta.java
@@ -2,17 +2,23 @@ package com.senac.model;
 
 public class Pergunta{
     private String pergunta;
+    private String[] alternativas;
     private String resposta;
     private String fase;
 
-    public Pergunta(String pergunta, String resposta, String fase) {
+    public Pergunta(String pergunta, String[] alternativas, String resposta, String fase) {
         this.pergunta = pergunta;
+        this.alternativas = alternativas;
         this.resposta = resposta.toLowerCase();
         this.fase = fase;
     }
 
     public String getPergunta() {
         return pergunta;
+    }
+
+    public String[] getAlternativas() {
+        return alternativas;
     }
 
     public String getResposta(){
@@ -24,6 +30,6 @@ public class Pergunta{
     }
 
     public boolean verificarResposta(String respostaUsuario) {
-        return this.resposta.equals(respostaUsuario.toLowerCase());
+        return this.resposta.equalsIgnoreCase(respostaUsuario);
     }
 }
